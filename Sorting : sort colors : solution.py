e1103076b1f162c819949fc96c56d6e987d4b8f0
Arun <arunnn101010@gmail.com>
2026-08-05 00:17:59 +0530

    Update Sorting : sort colors : solution.py

--- Sorting : sort colors : solution.py
+++ Sorting : sort colors : solution.py
@@ -1,1 +1,26 @@
+# Promblem - sort colors 
+# Approach - dutch national flag ( 3 pointers ) 
+# Time and space complexity - 0(n) & 0(1) 
+# Leetcode and diffculty level - 75 & easy 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0;
+        int high = nums.size() - 1;
 
+        while(mid <= high ) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
