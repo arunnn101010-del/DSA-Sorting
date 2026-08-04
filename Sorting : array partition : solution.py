489c34600eccc3d2962d1ccfbf2354eef6b436b1
Arun <arunnn101010@gmail.com>
2026-08-05 00:32:34 +0530

    Update Sorting : array partition : solution.py

--- Sorting : array partition : solution.py
+++ Sorting : array partition : solution.py
@@ -1,1 +1,19 @@
+# Promblem - array partition 
+# Approach - sorting 
+# Time and space complexity - 0(n) & 0(1) 
+# Leetcode and diffculty level - 561 & easy 
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        int sum = 0;
+
+        for(int i = 0; i<nums.size(); i+=2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
 
