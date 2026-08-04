ffcfc13bc86d73a97a1c741deceb588ba0c1d59b
Arun <arunnn101010@gmail.com>
2026-08-04 23:19:16 +0530

    Update Sorting : merge sorted array : solution.py

--- Sorting : merge sorted array : solution.py
+++ Sorting : merge sorted array : solution.py
@@ -1,1 +1,22 @@
+# Promblem - merge sorted array 
+# Approach - merge sort 
+# Time and space complexity - 0(m+n) & 0(n) 
+# Leetcode and diffculty level - 88 & easy 
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int idx = m+n-1, i = m-1 , j = n-1;
 
+        while(i >= 0 && j >= 0) {
+            if(A[i] >= B[j]) {
+                A[idx--] = A[i--];
+            } else {
+                A[idx--] = B[j--];
+            }
+        }
+
+        while( j >= 0) {
+            A[idx--] = B[j--];
+        }
+    }
+};
