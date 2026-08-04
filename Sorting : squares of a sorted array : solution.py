64e42981af7696c3b66a9ac753c1def0624dacb9
Arun <arunnn101010@gmail.com>
2026-08-05 00:00:54 +0530

    Update Sorting : squares of a sorted array : solution.py

--- Sorting : squares of a sorted array : solution.py
+++ Sorting : squares of a sorted array : solution.py
@@ -1,1 +1,27 @@
+# Promblem - squares of a sorted array 
+# Approach - two pointers 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 977 & easy 
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int idx = nums.size() - 1;
 
+        vector<int> ans(nums.size());
+
+        while(l <= r) {
+
+            if(abs(nums[l]) > abs(nums[r])) {
+                ans[idx--] = nums[l] * nums[l];
+                l++;
+            }
+            else {
+                ans[idx--] = nums[r] * nums[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
