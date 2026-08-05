a5484b6795fa4582b87ed3991b15cf9ac3b8dbf1
Arun <arunnn101010@gmail.com>
2026-08-05 22:54:06 +0530

    Update Sorting : kth largest element in a array : solution.py

--- Sorting : kth largest element in a array : solution.py
+++ Sorting : kth largest element in a array : solution.py
@@ -1,1 +1,20 @@
+# Promblem - kth largest element in a array 
+# Approach - heap sort
+# Time and space complexity - 0(n log k) & 0(k) 
+# Leetcode and diffculty level - 215 & medium 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
 
+        for(int num : nums) {
+            pq.push(num);
+
+            if(pq.size() > k) {
+                pq.pop();
+
+            }
+        }
+        return pq.top();
+    }
+};
