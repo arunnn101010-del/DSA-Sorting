84eae1ab044ec9c9606ee0cd4460341ecb4bb2f3
Arun <arunnn101010@gmail.com>
2026-08-05 22:41:10 +0530

    Update Sorting : largest number : solution.py

--- Sorting : largest number : solution.py
+++ Sorting : largest number : solution.py
@@ -1,1 +1,28 @@
+# Promblem - largest number 
+# Approach - custom sort 
+# Time and space complexity - 0(n log n * k ) & 09n0 
+# Leetcode and diffculty level - 179 & medium class Solution {
+public:
+    static bool cmp(string a, string b) {
+        return a+b > b+a;
+    }
 
+    string largestNumber(vector<int>& nums) {
+        vector<string> arr;
+        for(int num : nums) {
+            arr.push_back(to_string(num));
+        }
+
+        sort(arr.begin(), arr.end(), cmp);
+
+        if(arr[0] == "0") {
+            return "0";
+        }
+        string ans = "";
+
+        for(string s : arr) {
+            ans += s;
+        }
+        return ans;
+    }
+};
