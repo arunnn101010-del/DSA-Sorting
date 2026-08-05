4739efa78f519e7e879b73b7303886134250fa0f
Arun <arunnn101010@gmail.com>
2026-08-05 12:45:34 +0530

    Update Sorting : merge intervals : solution.py

--- Sorting : merge intervals : solution.py
+++ Sorting : merge intervals : solution.py
@@ -1,1 +1,23 @@
+# Promblem - merge intervals 
+# Approach - merge sort
+# Time and space complexity - 0(n log n) & 0(n) 
+# Leetcode and diffculty level - 56 & medium 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
 
+        sort(intervals.begin(), intervals.end());
+
+        ans.push_back(intervals[0]);
+
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
