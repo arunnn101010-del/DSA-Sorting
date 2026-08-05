0d84ded81036d2fb152e6af111271ab7d2b80238
Arun <arunnn101010@gmail.com>
2026-08-05 23:16:39 +0530

    Update Sorting : h-index : solution.py

--- Sorting : h-index : solution.py
+++ Sorting : h-index : solution.py
@@ -1,1 +1,21 @@
+# Problem - h-index 
+# Approach - counting sort 
+# Time and space complexity - 0(n log n) & 0(1) 
+# Leetcode and diffculty level - 274 & medium 
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
 
+        sort(citations.begin(), citations.end());
+
+        int n = citations.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(citations[i] >= n - i)
+                return n - i;
+        }
+
+        return 0;
+    }
+};
