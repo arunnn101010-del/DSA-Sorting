512ee6b0262533e03c93c5dbbd9c4e63c0bd35bc
Arun <arunnn101010@gmail.com>
2026-08-05 12:28:19 +0530

    Update Sorting : third maximum number : solution.py

--- Sorting : third maximum number : solution.py
+++ Sorting : third maximum number : solution.py
@@ -1,1 +1,26 @@
+# Promblem - third maximum number 
+# Approach - sorting 
+# Time and space complexity - 0(n log n) & 0(n) 
+# Leetcode and diffculty level - 414 & easy 
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
 
+        set<int> st;
+
+        for(int num : nums) {
+            st.insert(num);
+        }   
+        
+        if(st.size() < 3) {
+            return *st.rbegin();
+        }
+
+        auto it = st.rbegin();
+
+        it++;
+        it++;
+
+        return *it;
+    }
+};
